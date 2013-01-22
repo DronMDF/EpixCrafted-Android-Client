7c73a7c63018ce5f67ff83fae0b66abab2032b39
Andrey Valyaev <dron.valyaev@gmail.com>
2013-01-23 00:06:03 +0400

    Login dialog (dirty)

--- src/com/mdf/epix_agent/EpixAgent.java
+++ src/com/mdf/epix_agent/EpixAgent.java
@@ -1,15 +1,23 @@
 package com.mdf.epix_agent;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class EpixAgent extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		LayoutInflater inflater = getLayoutInflater();
+		builder.setView(inflater.inflate(R.layout.login_dialog, null));
+		Dialog dialog = builder.create();
+		dialog.show();
+	}
 }
